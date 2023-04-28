ef91295d3cbfc005d86dbbed0ea1325472874bfd
wenF7752 <wfang9@asu.edu>
2023-04-28 00:02:14 -0700

    problem 13

--- main.js
+++ main.js
@@ -64,3 +64,144 @@ const capitalizeFirstLetter = (str) => {
 		.map((word) => word[0].toUpperCase() + word.slice(1))
 		.join(' ')
 }
+
+const problem_6 = document.getElementById('problem6__input')
+const btn_6 = document.getElementById('btn__6')
+const answer_6 = document.getElementById('answer__6')
+btn_6.addEventListener('click', () => {
+	answer_6.innerHTML = longestWord(problem_6.value)
+	console.log('clicked')
+})
+const longestWord = (str) => {
+	let words = str.split(' ')
+	let longest = ''
+	for (let word of words) {
+		if (word.length > longest.length) longest = word
+	}
+	return longest
+}
+const problem_7 = document.getElementById('problem7__input')
+const btn_7 = document.getElementById('btn__7')
+const answer_7 = document.getElementById('answer__7')
+btn_7.addEventListener('click', () => {
+	answer_7.innerHTML = countVowels(problem_7.value)
+	console.log('clicked')
+})
+const countVowels = (str) => {
+	let count = 0
+	let vowels = ['a', 'e', 'i', 'o', 'u']
+	for (let char of str) {
+		if (vowels.includes(char)) count++
+	}
+	return count
+}
+
+const problem_8 = document.getElementById('problem8__input')
+const btn_8 = document.getElementById('btn__8')
+const answer_8 = document.getElementById('answer__8')
+btn_8.addEventListener('click', () => {
+	answer_8.innerHTML = isPrime(problem_8.value)
+	console.log('clicked')
+})
+const isPrime = (num) => {
+	if (num <= 1) return false
+	for (let i = 2; i < num; i++) {
+		if (num % i === 0) return false
+	}
+	return true
+}
+
+// let person = {
+// 	name: 'John'
+// }
+
+const problem_9 = document.getElementById('problem9__input')
+const btn_9 = document.getElementById('btn__9')
+const answer_9 = document.getElementById('answer__9')
+btn_9.addEventListener('click', () => {
+	answer_9.innerHTML = getType(problem_9.value)
+	console.log('clicked')
+})
+const getType = (value) => {
+	return typeof value
+}
+
+const problem_10 = document.getElementById('problem10__input')
+const btn_10 = document.getElementById('btn__10')
+const answer_10 = document.getElementById('answer__10')
+btn_10.addEventListener('click', () => {
+	answer_10.innerHTML = ''
+	matrix(problem_10.value).map((row) => {
+		answer_10.innerHTML += row.join(' ') + '<br>'
+	})
+	console.log('clicked')
+})
+const matrix = (n) => {
+	const matrix = []
+	for (let i = 0; i < n; i++) {
+		const row = []
+		for (let j = 0; j < n; j++) {
+			if (i === j) {
+				row.push(1)
+			} else {
+				row.push(0)
+			}
+		}
+		matrix.push(row)
+	}
+	return matrix
+}
+
+const problem_11 = document.getElementById('problem11__input')
+const btn_11 = document.getElementById('btn__11')
+const answer_11 = document.getElementById('answer__11')
+btn_11.addEventListener('click', () => {
+	answer_11.innerHTML = secondLowestAndGreatest(problem_11.value)
+	console.log('clicked')
+})
+const secondLowestAndGreatest = (arr) => {
+	let result = []
+	if (arr.length < 2) return 'array too small'
+	if (arr.includes(',')) {
+		result = arr.split(',')
+	} else {
+		result = arr.split(' ')
+	}
+	result = result.sort((a, b) => a - b)
+	return `${result[1]}, ${result[result.length - 2]}`
+}
+
+const problem_12 = document.getElementById('problem12__input')
+const btn_12 = document.getElementById('btn__12')
+const answer_12 = document.getElementById('answer__12')
+btn_12.addEventListener('click', () => {
+	answer_12.innerHTML = perfectNumber(problem_12.value)
+	console.log('clicked')
+})
+
+const perfectNumber = (num) => {
+	let number = parseInt(num)
+	let sum = 0
+	for (let i = 1; i <= number / 2; i++) {
+		if (number % i === 0) {
+			sum += i
+		}
+	}
+	return sum === number && number !== 0
+}
+const problem_13 = document.getElementById('problem13__input')
+const btn_13 = document.getElementById('btn__13')
+const answer_13 = document.getElementById('answer__13')
+btn_13.addEventListener('click', () => {
+	answer_13.innerHTML = factorsOfPositiveInt(problem_13.value)
+	console.log('clicked')
+})
+const factorsOfPositiveInt = (num) => {
+	let result = []
+	for (let i = 1; i <= num; i++) {
+		if (num % i === 0) {
+			result.push(i)
+		}
+	}
+	return result
+}
